888264eed3f0a59962f4a0a769d3f205650b176c
MINO1020 <dlalsgh6772@gmail.com>
2024-11-20 10:45:43 +0900

    'mainpull'

--- routes/purchases.js
+++ routes/purchases.js
@@ -7,7 +7,13 @@ app.use(express.json());
 //get
 app.get("/purchases", async (req, res) => {
   try {
-    const purchase_list = await db.query("SELECT * FROM purchases;");
+    const purchase_list = await db.query("SELECT * FROM Purchases;");
+    console.log(purchase_list);
     res.json({ success: true, purchase_list: purchase_list });
-  } catch (error) {}
+  } catch (error) {
+    console.error("error fetching purchases: ", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to fetch purchases." });
+  }
 });
