c5056117b069a9d2059b10e809b65216f3d896ac
inhooinu <inhooinu@gmail.com>
2024-11-25 10:04:53 +0900

    feat: Add 'phone' and 'role' fields to User table (#9)

--- models/user.js
+++ models/user.js
@@ -9,26 +9,38 @@ module.exports = function (sequelize, DataTypes) {
 				allowNull: false,
 				primaryKey: true,
 			},
+
 			id: {
 				type: DataTypes.STRING(15),
 				allowNull: false,
 				unique: true, // UNIQUE 제약 조건 추가
 			},
-			birth: {
-				type: DataTypes.DATEONLY,
+			password: {
+				type: DataTypes.STRING(255),
 				allowNull: false,
 			},
 			username: {
 				type: DataTypes.STRING(255),
 				allowNull: false,
 			},
-			password: {
-				type: DataTypes.STRING(255),
+			birth: {
+				type: DataTypes.DATEONLY,
 				allowNull: false,
+			},
+			phone: {
+				type: DataTypes.STRING(15), // 휴대폰 번호, 길이 15로 제한
+				allowNull: false,
+				validate: {
+					is: /^[0-9]{10,15}$/,
+				},
 			},
 			school_auth: {
 				type: DataTypes.STRING(255),
 				allowNull: true,
+			},
+			role: {
+				type: DataTypes.ENUM("parent", "child"), // 부모인지 자식인지
+				allowNull: false,
 			},
 			parent_id: {
 				type: DataTypes.BIGINT,
