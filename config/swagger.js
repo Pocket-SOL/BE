60ecde23c5d15420039f92d4de2525acedca4543
0321minji <0321minji@ewhain.net>
2024-11-25 13:47:41 +0900

    Fix : add server url on swagger

--- config/swagger.js
+++ config/swagger.js
@@ -2,28 +2,30 @@ const swaggerJsdoc = require("swagger-jsdoc");
 
 // Swagger 설정 정의
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Meditator's Node Express API with Swagger",
-      version: "0.1.0",
-      description: "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "Meditator",
-        url: "https://github.com/Pocket-SOL/BE",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000/",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"], // 주석에서 API 문서화 추출
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Meditator's Node Express API with Swagger",
+			version: "0.1.0",
+			description:
+				"This is a simple CRUD API application made with Express and documented with Swagger",
+			license: {
+				name: "MIT",
+				url: "https://spdx.org/licenses/MIT.html",
+			},
+			contact: {
+				name: "Meditator",
+				url: "https://github.com/Pocket-SOL/BE",
+			},
+		},
+		servers: [
+			{
+				url: "http://localhost:3000/",
+				url: "http://52.79.58.245:8080/",
+			},
+		],
+	},
+	apis: ["./routes/*.js"], // 주석에서 API 문서화 추출
 };
 
 // Swagger 사양 생성
